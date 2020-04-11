df4a6dcff3b26f67ea2afcaa7d25f9e61946bf56
Tobias Wittwer <t.wittwer95@gmail.com>
2020-04-11 23:28:29 +0200

    add storybook docs addon

--- libs/shared/storybook/.storybook/config.js
+++ libs/shared/storybook/.storybook/config.js
@@ -1,5 +1,13 @@
-import { configure, addDecorator } from '@storybook/angular';
+import { configure, addDecorator, addParameters } from '@storybook/angular';
 import { withKnobs } from '@storybook/addon-knobs';
+import { DocsContainer, DocsPage } from '@storybook/addon-docs/blocks';
+
+addParameters({
+  docs: {
+    container: DocsContainer,
+    page: DocsPage,
+  },
+});
 
 addDecorator(withKnobs);
 configure(require.context('../..', true, /\.stories\.tsx?$/), module);
